7de4e40d9ee366aeed2282e705eaff37651618d7
Balmiki kumar <88521022+Balmikiii@users.noreply.github.com>
2023-11-14 17:45:04 +0530

    login with session

---
+++ php/login/session/ses_end.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// remove all session variables
+session_unset();
+
+// destroy the session
+// session_destroy();
+
+if (isset($_SESSION['user'])) {
+    // Session exists, user is logged in, or you can perform any other action here
+} else {
+    // Session does not exist, user is not logged in
+    // Redirect to the index or login page
+    header('Location: ../index.html');
+    exit; // Make sure to exit to stop the script execution
+}
+
+?>
+
+</body>
+</html>
