e139c62567cd55854eb18e6a935640e5303c6ddd
Balmiki kumar <88521022+Balmikiii@users.noreply.github.com>
2023-11-14 16:28:03 +0530

    now successfully generate

--- php/custom primary  key/a.php
+++ php/custom primary  key/a.php
@@ -8,34 +8,28 @@
             mysqli_select_db($con,"cus_primary");
             
             $balmiki="CREATE TABLE IF NOT EXISTS msg (
-                custom_prefix VARCHAR(3) NOT NULL,
-                id int(11) NOT NULL auto_increment,
+                userid VARCHAR(300) NOT NULL,
+                id int(225) NOT NULL auto_increment,
                 name varchar(300)NOT NULL,
                 email varchar(300)NOT NULL,
                 msg varchar(255)NOT NULL,
                 PRIMARY KEY(id) )";
            $con->query($balmiki);
-
-           
            if(isset($_POST['submit'])){
                $name = $_POST['name'];
                $email = $_POST['email'];
                $comment = $_POST['msg'];
             }
-            
-            $mydb = "insert into msg (custom_prefix,name,email,msg)
-            value('STU','$name','$email','$comment')";
-            $query = mysqli_query ($con,$mydb);
-            echo "success your data send to balmiki";
-            // header('location:contact.php');
-            
-            // if (mysqli_query($con, $mydb)) {
-            //     $last_id = mysqli_insert_id($con);
-            //     echo "New record created successfully. Last inserted ID is: " . $last_id;
-            //   } else {
-            //     echo "Error: " . $sql . "<br>" . mysqli_error($con);
-            //   }
-
-            
+            $mydb = "insert into msg (name,email,msg)
+            value('$name','$email','$comment')";
+            if (mysqli_query($con, $mydb)) {
+                $last_id = mysqli_insert_id($con);
+                $userid= "STU_".$last_id;
+                $quer="UPDATE msg SET userid ='".$userid."' WHERE id= '".$last_id."'";                
+                $ress = mysqli_query($con, $quer);
+                echo "New record created successfully. Last inserted ID is: " . $userid;
+              } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($con);
+              }
 ?>
  
